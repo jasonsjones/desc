c378b1046bc77807d79d504305828f1f2be2bd65
Jason S. Jones <jsjones96@gmail.com>
2021-12-29 11:18:07 -0800

    feat: add client-side pagination to approver lists

--- packages/app/src/components/Inbox/ApproverItemList.js
+++ packages/app/src/components/Inbox/ApproverItemList.js
@@ -5,13 +5,8 @@ import { useQueryClient } from 'react-query';
 import ApproverActions from './ApproverActions';
 import NoteDetails from './NoteDetails';
 import AddNoteForm from './AddNoteForm';
-import {
-    inboxStyles as css,
-    initCollapsibleElements,
-    closedStatuses,
-    updateItemsWithNote
-} from './utils';
-import useFilterItemsByStatus from '../../hooks/useFilterItemsByStatus';
+import { inboxStyles as css, initCollapsibleElements, closedStatuses } from './utils';
+import Pagination from '../Pagination';
 
 const statusMap = {
     denied: 'Rejected',
@@ -32,8 +27,9 @@ const ListHeader = () => {
     );
 };
 
-const ApproverItemList = ({ filter, items }) => {
-    const [displayableItems, setDisplayableItems] = useFilterItemsByStatus(items, filter);
+const ApproverItemList = ({ filter, items, pageSize = 25 }) => {
+    const [pageNum, setPageNum] = useState(0);
+    const [displayableItems, setDisplayableItems] = useState([]);
     const queryClient = useQueryClient();
 
     const { updateItem } = useUpdateItem((response) => {
@@ -46,8 +42,32 @@ const ApproverItemList = ({ filter, items }) => {
         initCollapsibleElements(M);
     }, []);
 
+    useEffect(() => {
+        setDisplayableItems(
+            items.filter((item) => {
+                if (Array.isArray(filter)) {
+                    return filter.some((status) => status === item.status);
+                }
+                return item.status === filter;
+            })
+        );
+    }, [items, filter]);
+
     const handleNoteAdd = (itemId, itemData) => {
-        setDisplayableItems(displayableItems.map(updateItemsWithNote(itemId, itemData)));
+        setDisplayableItems(
+            displayableItems.map((item) => {
+                if (item.id === itemId) {
+                    return {
+                        ...item,
+                        updatedAt: itemData.updatedAt,
+                        notes: [...itemData.notes]
+                    };
+                }
+                return {
+                    ...item
+                };
+            })
+        );
     };
 
     const updateItemStatus = (id, status) => {
@@ -58,53 +78,88 @@ const ApproverItemList = ({ filter, items }) => {
         return closedStatuses.some((status) => status === item.status);
     };
 
+    const nextPage = () => {
+        if (pageNum + 1 < displayableItems.length / pageSize) {
+            setPageNum((num) => num + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (pageNum > 0) {
+            setPageNum((num) => num - 1);
+        }
+    };
+
+    const handlePageNumberClick = (num) => {
+        setPageNum(num);
+    };
+
     return (
-        <ul className="collapsible expandable">
-            <ListHeader />
-            {displayableItems &&
-                displayableItems.map((item) => {
-                    return (
-                        <li key={item.id}>
-                            <div className="collapsible-header" style={css.itemHeader}>
-                                <p style={css.flexItem}>
-                                    {item.submittedBy.name.first} {item.submittedBy.name.last}
-                                </p>
-                                <p style={css.flexItemCapitialize}>{item.name}</p>
-                                <p style={css.flexItem}>{item.quantity}</p>
-                                <p style={css.flexItem}>
-                                    {new Date(item.createdAt).toDateString()}
-                                </p>
-                            </div>
-                            <div className="collapsible-body">
-                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <ApproverActions
-                                        filter={filter}
-                                        item={item}
-                                        onStatusChange={updateItemStatus}
-                                    />
-                                    {isItemClosed(item) ? (
-                                        <span className="text-grey-lighten-1">
-                                            <em>{statusMap[item.status]}</em>
-                                        </span>
-                                    ) : null}
-                                </div>
-                                <p style={{ fontSize: '1.125rem', marginBottom: '.5rem' }}>
-                                    Notes:
-                                </p>
-                                {item.notes.map((note) => (
-                                    <NoteDetails key={note.id} note={note} />
-                                ))}
-                                <AddNoteForm itemId={item.id} onNoteAdd={handleNoteAdd} />
-                            </div>
-                        </li>
-                    );
-                })}
-            {(!displayableItems || displayableItems.length === 0) && (
-                <li>
-                    <div style={{ padding: '1rem' }}>No items available</div>
-                </li>
-            )}
-        </ul>
+        <>
+            <ul className="collapsible expandable">
+                <ListHeader />
+                {displayableItems &&
+                    displayableItems
+                        .slice(pageNum * pageSize, pageNum * pageSize + pageSize)
+                        .map((item) => {
+                            return (
+                                <li key={item.id}>
+                                    <div className="collapsible-header" style={css.itemHeader}>
+                                        <p style={css.flexItem}>
+                                            {item.submittedBy.name.first}{' '}
+                                            {item.submittedBy.name.last}
+                                        </p>
+                                        <p style={css.flexItemCapitialize}>{item.name}</p>
+                                        <p style={css.flexItem}>{item.quantity}</p>
+                                        <p style={css.flexItem}>
+                                            {new Date(item.createdAt).toDateString()}
+                                        </p>
+                                    </div>
+                                    <div className="collapsible-body">
+                                        <div
+                                            style={{
+                                                display: 'flex',
+                                                justifyContent: 'space-between'
+                                            }}
+                                        >
+                                            <ApproverActions
+                                                filter={filter}
+                                                item={item}
+                                                onStatusChange={updateItemStatus}
+                                            />
+                                            {isItemClosed(item) ? (
+                                                <span className="text-grey-lighten-1">
+                                                    <em>{statusMap[item.status]}</em>
+                                                </span>
+                                            ) : null}
+                                        </div>
+                                        <p style={{ fontSize: '1.125rem', marginBottom: '.5rem' }}>
+                                            Notes:
+                                        </p>
+                                        {item.notes.map((note) => (
+                                            <NoteDetails key={note.id} note={note} />
+                                        ))}
+                                        <AddNoteForm itemId={item.id} onNoteAdd={handleNoteAdd} />
+                                    </div>
+                                </li>
+                            );
+                        })}
+                {(!displayableItems || displayableItems.length === 0) && (
+                    <li>
+                        <div style={{ padding: '1rem' }}>No items available</div>
+                    </li>
+                )}
+            </ul>
+            {displayableItems.length > pageSize ? (
+                <Pagination
+                    numPages={displayableItems.length / pageSize}
+                    currentPage={pageNum + 1}
+                    nextPage={nextPage}
+                    prevPage={prevPage}
+                    onPageClick={handlePageNumberClick}
+                />
+            ) : null}
+        </>
     );
 };
 
