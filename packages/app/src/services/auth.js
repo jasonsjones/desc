536363b83c4e508746fd643879f545208e26623f
Jason Jones <jsjones96@gmail.com>
2022-04-15 09:20:53 -0700

    chore: remove client app useage of jwt pkg

--- packages/app/src/services/auth.js
+++ packages/app/src/services/auth.js
@@ -1,4 +1,3 @@
-import jwt from 'jsonwebtoken';
 import { BASE_URL } from './util';
 
 export function login(creds) {
@@ -33,21 +32,6 @@ export const fetchSessionUser = () => {
         credentials: 'include'
     }).then((response) => response.json());
 };
-
-export function getValidToken(currentToken) {
-    const tokenExpires = jwt.decode(currentToken).exp;
-    const now = Date.now().valueOf() / 1000;
-    if (now > tokenExpires) {
-        return refreshToken().then((data) => {
-            if (data.payload.accessToken) {
-                return data.payload.accessToken;
-            } else {
-                return '';
-            }
-        });
-    }
-    return Promise.resolve(currentToken);
-}
 
 export function refreshToken() {
     return fetch('http://localhost:3001/api/auth/refreshtoken', {
--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -3,19 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    define: {
-        "global": {},
-        "stream": {},
-        "process": {}
-    },
-    resolve: {
-        alias: {
-            inherits: './src/overrides/inherits.js'
-        }
-    },
     server: {
         port: 4200
     },
     plugins: [react()]
 });
-
