88ca5cea756dbf4a76ae9cca6d36a879e179da67
Jason Jones <jsjones96@gmail.com>
2022-08-27 13:06:42 -0700

    chore: update app dev deps

--- packages/app/src/App.test.jsx
+++ packages/app/src/App.test.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
+
+jest.mock('./hooks/useRefreshAccessToken', () => ({
+    __esModule: true,
+    default: () => jest.fn()
+}));
 
 afterEach(cleanup);
 
 it('renders without crashing', () => {
-    const { container } = render(<App />);
-    expect(container).toBeTruthy();
+    render(<App />);
+    expect(screen.getByText(/welcome to the desc portal/i)).toBeInTheDocument();
 });
